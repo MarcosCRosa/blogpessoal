81c595c9f4b7365c5e6d01af8c94bd84242a6c7e
MarcosCRosa <marcosluiz4012@gmail.com>
2025-12-03 11:34:53 -0300

    feat: implementado CRUD para postagem.

--- src/postagem/services/postagem.service.ts
+++ src/postagem/services/postagem.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from "@nestjs/common";
-import { Repository } from "typeorm";
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
+import { ILike, Repository } from "typeorm";
 import { Postagem } from "../entities/Postagem.entity";
 import { InjectRepository } from "@nestjs/typeorm";
+import { DeleteResult } from "typeorm/browser";
 
 @Injectable()
 export class PostagemService{
@@ -11,7 +12,42 @@ constructor(
 
   async findAll(): Promise<Postagem[]>{
     return await this.postagemRepository.find();
+  }
+  
+  async findById(id:number): Promise<Postagem>{
+    const postagem = await this.postagemRepository.findOne({
+      where: {
+        id,
+      },
+    });
+
+       if(!postagem)
+       throw new HttpException('Postagem não encontrada!',HttpStatus.NOT_FOUND);
+    
+    return postagem;
   } 
+
+  async create(postagem:Postagem): Promise<Postagem>{
+    return await this.postagemRepository.save(postagem);  
+  }
+
+  async update(postagem:Postagem): Promise<Postagem>{
+    await this.findById(postagem.id)
+
+    return await this.postagemRepository.save(postagem) 
+ }
+
+  async delete(id:number):Promise<DeleteResult>{
+    await this.findById(id);
+
+    return await this.postagemRepository.delete(id)
+  }
+  
+  async findAllByTitulo(titulo:string):Promise <Postagem[]>{
+    return await this.postagemRepository.find({
+      where:{titulo:ILike(`%${titulo}%`)}
+    })
+  }
 }
 
 
