0943667518723547ce50e21cb26c201bd36530b2
MarcosCRosa <marcosluiz4012@gmail.com>
2025-12-16 13:17:14 -0300

    feat: documentacao com swagger e testes para usuario utilizando jest

--- src/usuario/entities/usuario.entity.ts
+++ src/usuario/entities/usuario.entity.ts
@@ -1,27 +1,36 @@
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 import { Postagem } from "../../postagem/entities/Postagem.entity";
 import { IsEmail, IsNotEmpty, MinLength } from "class-validator";
+import { ApiProperty } from "@nestjs/swagger";
 
 @Entity({name:"tb_usuario"})
 export class Usuario {
-
+    @ApiProperty()
     @PrimaryGeneratedColumn()
     id: number;
-
+    
+    @ApiProperty()
     @IsNotEmpty()
     @Column({length:255,nullable:false})
     nome: string;
+
+    @ApiProperty({example: 'email@email.com.br'})
     @IsEmail()
     @IsNotEmpty()
     @Column({length:255,nullable:false})
     usuario: string;
+
+    @ApiProperty()
     @MinLength(8)
     @IsNotEmpty()
     @Column({length:255,nullable:false})
     senha: string;
-
+     
+    @ApiProperty()
     @Column({length:5000})
     foto: string;
+
+    @ApiProperty()
     @OneToMany(()=>Postagem,(postagem)=>postagem.usuario)
     postagem: Postagem[];
 
